dfa65cc90d14cce8cd4cf1070f058978d84cd75c
ParkHyunseo <soilove2@naver.com>
2019-11-16 21:39:27 +0900

    feat: potato component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Potato from "./Potato";
 
 function App() {
   return (
-    <div className="App" />
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+
   );
 }
 
