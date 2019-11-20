4e98003d0d69543006b90604d6e73dace266986e
Tetramad <cbwjd777@naver.com>
2019-11-20 18:12:22 +0900

    feat: add prop-types package and apply it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropType from 'prop-types';
+
+function Coffee({ fav }) {
+    return <h1>I love {fav} coffee</h1>;
+}
+
+Coffee.propTypes = {
+    fav: PropType.string.isRequired
+}
+
+const coffeeILove = [
+    {
+        id: 0,
+        name: "MAX"
+    },
+    {
+        id: 1,
+        name: "Pandorothy"
+    },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {coffeeILove.map(coffee => <Coffee key={coffee.id} fav={coffee.name} />)}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MaxCoffee.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MaxCoffee() {
+    return <h3>I love MAX coffee</h3>;
+}
+
+export default MaxCoffee;
