1a645400c9840c39f329892ba2376e7a603641cc
ParkHyunseo <soilove2@naver.com>
2019-11-22 22:01:45 +0900

    feat: Learn react property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Potato( { favorite }){
+  return <h1>i like {favorite} </h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Potato favorite = "Kimchi"/>
     </div>
 
   );
