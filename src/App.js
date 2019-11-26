8e42d371b9c98d959161e928835976f6bcd2dd2f
ParkHyunseo <soilove2@naver.com>
2019-11-26 21:48:32 +0900

    feat: #3.3 Planing the Moview Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 class App extends React.Component
 {
-
   state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState(current => ({ count : current.count + 1}))
-  };
-  minus = () => {
-    this.setState(current => ({ count : current.count - 1}));
-  };
+    isLoading:true,
+    movies:[],
+  }
 
+  componentDidMount()
+  {
+    setTimeout(() => {
+      this.setState({isLoading:false});
+    }, 6000);
+  }
 
   render(){
+    const {isLoading} = this.state;
     return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>minus</button>
-    </div>
+      <h1>{isLoading ? "is Loading..." : "The Movies"} </h1>
     )
   }
 }
