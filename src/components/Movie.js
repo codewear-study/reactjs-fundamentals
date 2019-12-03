98b71d3628b01b33b576e49919104d28ca81fa80
ParkHyunseo <soilove2@naver.com>
2019-12-03 23:36:44 +0900

    feat: #6.4 Redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({id, year, title, summary, poster, genres }) {
   return (
-    <Link
+    <div className="movie">
+      <Link
       to={{
-        pathname: "/movie-detail",
+        pathname: `/movie/${id}`,
         state: {
           year,
           title,
@@ -16,23 +17,22 @@ function Movie({ year, title, summary, poster, genres }) {
           genres
         }
       }}
-    >
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...</p>
-      </div>
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
-    </Link>
   );
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Detail(props){
-    return <h1>1</h1>
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location, history } = this.props;
+        if(location.state === undefined) {
+            history.push("/");
+        }
+    }
+
+    render() {
+        const { location } = this.props; 
+        if(location.state) {
+            return <span>{location.state.title}</span>;
+        } else {
+            return null;
+        }
+    }
+    
 }
 
 export default Detail;
